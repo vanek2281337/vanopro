401e4194633c02dfa728c42ebdcaab6f1dc7820a
HypocriteM41 <rYverM41@gmail.com>
2023-10-11 13:54:19 +0700

    *

--- vanopro/Pages/MainPage.xaml.cs
+++ vanopro/Pages/MainPage.xaml.cs
@@ -2,13 +2,31 @@
 {
     public partial class MainPage : ContentPage
     {
+        private string _phoneEntry { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
         }
+        private bool is_validPhoneNumber()
+        {
+            if (_phoneEntry == null && _phoneEntry.Length <18)
+            {
 
+                DisplayAlert("", "Неверный номер телефона!", "Ok");
+                return false;
+            }
+            return true;
+        }
+        private void send_Clicked(object sender, EventArgs e)
+        {
+            _phoneEntry = PhoneEntry.Text;
+            if (is_validPhoneNumber ())
+            {
+                CodeEntry.IsVisible = true;
 
-       
+            
+            }
+        }
     }
 }
